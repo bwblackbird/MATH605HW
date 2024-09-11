5073cdb24be3700baa347adaf97550836487c10e
cabbidge <121267327+cabbidge@users.noreply.github.com>
2024-09-11 17:45:12 -0500

    Exercise 2.8.7

---
+++ HW1 9.12.24/ex2.8.7.R
@@ -0,0 +1,7 @@
+# Q: Briefly explain how it is entirely possible that 95% of the observations
+# fall outside of the 95% CI.
+
+# A: The confidence interval only gives a range of where the true mean 
+# is likely to be. In sample data with a high variance, it is possible that
+# 95% of observations will lie outside of the 95% confidence interval, since
+# the 95% CI only describes where the true mean is likely to be 95% of the time.
