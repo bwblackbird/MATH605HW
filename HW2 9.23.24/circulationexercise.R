b10fcadd27dc7a23a9f0fdff315667f8151fb954
cabbidge <121267327+cabbidge@users.noreply.github.com>
2024-09-20 22:29:31 -0500

    init HW2

---
+++ HW2 9.23.24/circulationexercise.R
@@ -0,0 +1,6 @@
+# Data for weekday and Sunday circulation of 89 US newspapers are
+# given in circulation.txt. We are interested in predicting the Sunday 
+# circulation of a newspaper that has a weekday circulation of 210,000.
+
+circ <- read.table('HW2 9.23.24/circulation.txt', header=TRUE, sep='\t')
+circ
