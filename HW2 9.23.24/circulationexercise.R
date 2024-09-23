5ee0ff4c8de6489ad86806ab3d399233fa3630cd
cabbidge <121267327+cabbidge@users.noreply.github.com>
2024-09-23 16:49:59 -0500

    Finish circulation exercise

--- HW2 9.23.24/circulationexercise.R
+++ HW2 9.23.24/circulationexercise.R
@@ -4,8 +4,32 @@
 
 circ <- read.table('HW2 9.23.24/circulation.txt', header=TRUE, sep='\t')
 plot(Sunday ~ Weekday, data=circ)
-model <- lm(Sunday ~ Weekday, data=circ)
-summary(model)
-abline(model)
-plot(model)
+model_fit <- lm(Sunday ~ Weekday, data=circ)
+summary(model_fit)
+abline(model_fit)
+plot(model_fit)
+
+# find leverage values greater than 4/n
+n <- nrow(circ)
+lev_vals <- hatvalues(model_fit)
+lev_threshold <- 4 / n
+high_lev_points <- which(leverage_vals > threshold)
+
+
+# find  standardized residuals with absolute value greater than 2
+residuals <- rstandard(model_fit)
+outliers <- which(abs(residuals) > 2)
+
+#find high leverage points that are not outliers
+high_lev_not_outliers <- setdiff(high_lev_points, high_residuals)
+
+# find cook's distance greater than 4/(n-2)
+cooks_distances <- cooks.distance(model_fit)
+cooks_threshold <- 4 / (n - 2)
+high_cooks <- which(cooks_distances > cooks_threshold)
+
+cat("High Leverage Points: ", high_lev_points, "\n")
+cat("Outliers: ", outliers, "\n")
+cat("High Leverage Points that are not Outliers: ", high_lev_not_outliers, "\n")
+cat("High Cooks Distances: ", high_cooks, "\n")
 
